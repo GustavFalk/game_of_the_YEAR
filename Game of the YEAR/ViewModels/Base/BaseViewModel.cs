564cbc15c3d2c3c940657d4d91307731ae863948
Philip Bruhn <philip.bruhn@gmail.com>
2020-09-11 10:03:37 +0200

    Skapat game page och gjort några ändirngar i Mainwindow och lagt till font.

---
+++ Game of the YEAR/ViewModels/Base/BaseViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace Game_of_the_YEAR.ViewModels.Base
+{
+    class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}

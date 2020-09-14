0dc9c31a0cca33f8863bcd9000a7a274abee423b
Love Lindqvist <lindqvistlove@hotmail.com>
2020-09-14 21:40:40 +0200

    HighScoreViewmodel

--- Game of the YEAR/MainWindow.xaml.cs
+++ Game of the YEAR/MainWindow.xaml.cs
@@ -22,7 +22,7 @@ namespace Game_of_the_YEAR
     /// </summary>
     public partial class MainWindow : Window
     {
-        Page page = new Views.RulePage();
+        Page page = new Views.HighScorePage();
         //int test;
         public MainWindow()
         {
---
+++ Game of the YEAR/ViewModels/HighScoreViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Game_of_the_YEAR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+
+namespace LovesTestGitHub
+{
+    class HighScoreViewModel : INotifyPropertyChanged
+    {
+        public string HighScore1Player { get; set; }
+        public string HighScore2Player { get; set; }
+        public string HighScore3Player { get; set; }
+        public string HighScore4Player { get; set; }
+        public string HighScore5Player { get; set; }
+        public string HighScore1Points { get; set; }
+        public string HighScore2Points { get; set; }
+        public string HighScore3Points { get; set; }
+        public string HighScore4Points { get; set; }
+        public string HighScore5Points { get; set; }
+        public ICommand ButtonCommand { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void InputToOutput()
+        {
+            HighScore1Player = "NVM";
+            HighScore2Player = "BOB";
+            HighScore3Player = "SUD";
+            HighScore4Player = "LOL";
+            HighScore5Player = "BRU";
+            HighScore1Points = "98992";
+            HighScore2Points = "77948";
+            HighScore3Points = "71993";
+            HighScore4Points = "58448";
+            HighScore5Points = "22291";
+
+        }
+
+        public HighScoreViewModel()
+        {
+            ButtonCommand = new RelayCommand(InputToOutput);
+        }
+    }
+}

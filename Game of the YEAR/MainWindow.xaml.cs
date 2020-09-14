9027c57e46f487016a3ab0ccdfcaddf0cd304acd
Magnus Hjelte <mahj1901@student.miun.se>
2020-09-14 14:30:29 +0200

    Skapat page för befintlig spelare

--- Game of the YEAR/MainWindow.xaml.cs
+++ Game of the YEAR/MainWindow.xaml.cs
@@ -22,7 +22,7 @@ namespace Game_of_the_YEAR
     /// </summary>
     public partial class MainWindow : Window
     {
-        Page page = new Views.StartpagePage();
+        Page page = new Views.ExistingUserPage();
         //int test;
         public MainWindow()
         {

5791442d930fb5b5cc657b1c584e989790ba8f0f
Philip Bruhn <philip.bruhn@gmail.com>
2020-09-14 09:49:28 +0200

    Fixat input i textboxar och så att dessa parse samt lagt till label som visas om detta inte funkar.

--- Game of the YEAR/ViewModels/GamePageViewModel.cs
+++ Game of the YEAR/ViewModels/GamePageViewModel.cs
@@ -2,21 +2,36 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace Game_of_the_YEAR.ViewModels
 {
     class GamePageViewModel:Base.BaseViewModel
     {
+        #region Properties
+
         public int Points { get; set; } = 99999;
         public bool CountDown { get; set; } = true;
         public string Hint { get; set; }
+        public string Number1 { get; set; }
+        public string Number2 { get; set; }
+        public string Number3 { get; set; }
+        public string Number4 { get; set; }
+        public int Answer { get; set; }
+        public Visibility NotNumberVisibility { get; set; } = Visibility.Hidden;
         public ICommand OKCommand { get; set; }
+
+        #endregion
+
+        #region Constructors
         public GamePageViewModel()
         {
             OpenPage();
-            OKCommand = new RelayCommand(StopCountDown);
+            OKCommand = new RelayCommand(OKButton);
         }
+
+        #endregion
 
         #region Points Counter Methods
 
@@ -60,11 +75,34 @@ namespace Game_of_the_YEAR.ViewModels
 
         #endregion
 
+
         public async void OpenPage()
         {
             await TypeClueSlower();
             
             CountDownPoints();
         }
+
+        public bool MakeAnswerToInt()
+        {
+            bool isNumber;
+            int intAnswer;
+            string stringAnswer = $"{Number1}{Number2}{Number3}{Number4}";
+            isNumber = int.TryParse(stringAnswer, out intAnswer);
+            Answer = intAnswer;
+            return isNumber;
+        }
+        public void OKButton()
+        {
+            if (MakeAnswerToInt() == false)
+            {
+                NotNumberVisibility = Visibility.Visible;
+            }
+            else
+            {
+                StopCountDown();
+            }
+        }
+            
     }
 }

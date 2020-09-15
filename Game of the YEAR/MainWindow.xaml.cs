9b0f916c239e4195560b23b6473e4e88f5a0a4de
Gustav <Gustav@DESKTOP-PTNUM16>
2020-09-15 10:52:14 +0200

    GJort klart viewmodel till Create user

--- Game of the YEAR/MainWindow.xaml.cs
+++ Game of the YEAR/MainWindow.xaml.cs
@@ -22,7 +22,7 @@ namespace Game_of_the_YEAR
     /// </summary>
     public partial class MainWindow : Window
     {
-        Page page = new Views.StartpagePage();
+        Page page = new Views.CreateUserPage();
         //int test;
         public MainWindow()
         {
--- Game of the YEAR/ViewModels/CreateUserPageViewModel.cs
+++ Game of the YEAR/ViewModels/CreateUserPageViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace Game_of_the_YEAR.ViewModels
@@ -8,14 +9,35 @@ namespace Game_of_the_YEAR.ViewModels
     class CreateUserPageViewModel: Base.BaseViewModel
 
     {
+        #region models
+        public void GoToStartGamePage()
+        {
+            Page page = new Views.StartGamePage();
+            ((MainWindow)System.Windows.Application.Current.MainWindow).Main.Content = page;
+        }
+
+        public void ValuesToUserID()
+        {
+            UserIdTxt = $"{Value1}{Value2}{Value3}";
+        }
+    
+        #endregion
+
+
         public string EmailTxt { get; set; }
-        public int UserIdTxt { get; set; }
+        public string UserIdTxt { get; set; }
+        public string Value1 { get; set; }
+        public string Value2 { get; set; }
+        public string Value3 { get; set; }
+
 
         public ICommand CreateUserBtn { get; set; }
 
         public CreateUserPageViewModel() 
         {
-
+            CreateUserBtn = new RelayCommand(ValuesToUserID);
         }
+
+       
     }
 }

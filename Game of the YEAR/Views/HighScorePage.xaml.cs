f5712776b673ebf418c87790d65616147b6e5609
Love Lindqvist <lindqvistlove@hotmail.com>
2020-09-15 16:17:53 +0200

    HighScore, andra försöker. Bara till LoveL

---
+++ Game of the YEAR/Views/HighScorePage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Game_of_the_YEAR.Views
+{
+    /// <summary>
+    /// Interaction logic for HighScorePage.xaml
+    /// </summary>
+    public partial class HighScorePage : Page
+    {
+        public HighScorePage()
+        {
+            InitializeComponent();
+        }
+    }
+}

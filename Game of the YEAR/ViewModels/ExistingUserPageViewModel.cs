702835eaaa60f12d5576e46d748983735d9a54e1
Gustav <Gustav@DESKTOP-PTNUM16>
2020-09-15 11:21:47 +0200

    skapat vymodell till Existing User

---
+++ Game of the YEAR/ViewModels/ExistingUserPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Game_of_the_YEAR.ViewModels
+{
+    class ExistingUserPageViewModel
+    {
+        public string EmailTxt { get; set; }
+
+        public ICommand LogInBtn { get; set; }
+
+        public ExistingUserPageViewModel()
+        {
+
+        }
+    }
+}

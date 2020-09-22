db1d24f6eb95a17996c854b54c3ffb223112be06
Magnus Hjelte <mahj1901@student.miun.se>
2020-09-22 15:22:28 +0200

    Pillat med designen på Highscorelistan

--- Game of the YEAR/MainWindow.xaml.cs
+++ Game of the YEAR/MainWindow.xaml.cs
@@ -23,7 +23,7 @@ namespace Game_of_the_YEAR
     /// </summary>
     public partial class MainWindow : Window
     {
-        Page page = new Views.LoadingPageOne();
+        Page page = new Views.HighScorePage();
         public MediaPlayer mediaPlayer = new MediaPlayer();
         
 

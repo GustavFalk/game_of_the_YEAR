440359268d824debe0b4b5928e60e1c3df726ab1
Magnus Hjelte <mahj1901@student.miun.se>
2020-09-29 15:24:00 +0200

    Mer Clean Code

--- Game of the YEAR/Models/Player.cs
+++ Game of the YEAR/Models/Player.cs
@@ -6,10 +6,11 @@ namespace Game_of_the_YEAR.Models
 {
     public class Player
     {
+        #region Properties
         public Int64 PlayerID { get; set; }
         public string Nickname { get; set; }
         public string Email { get; set; }
-        
+        #endregion
 
     }
 }
--- Game of the YEAR/Models/Question.cs
+++ Game of the YEAR/Models/Question.cs
@@ -7,14 +7,16 @@ namespace Game_of_the_YEAR.Models
 {
     public class Question
     {
+        #region Properties
         public int Year { get; set; }
         public List<string> Clues { get; set; }
-
+        #endregion
+        #region Constructor
         public Question(int year)
         {
             Year = year;
             Clues = GetClues(Year);
         }
-
+        #endregion
     }
 }
